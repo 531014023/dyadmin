e989ed1f936b9e47cc889fadcb0b93f79d752904
dy <531014023@qq.com>
2017-12-14 15:58:04 +0800

    无用文件删除

---
+++ .phpstorm.meta.php
@@ -0,0 +1,19 @@
+<?php
+	namespace PHPSTORM_META {
+	/** @noinspection PhpUnusedLocalVariableInspection */
+	/** @noinspection PhpIllegalArrayKeyTypeInspection */
+	$STATIC_METHOD_TYPES = [
+
+		\D('') => [
+			'Menu' instanceof Admin\Model\MenuModel,
+			'Adv' instanceof Think\Model\AdvModel,
+			'Mongo' instanceof Think\Model\MongoModel,
+			'View' instanceof Think\Model\ViewModel,
+			'Log' instanceof Admin\Model\LogModel,
+			'Relation' instanceof Think\Model\RelationModel,
+			'Base' instanceof Common\Model\BaseModel,
+			'Admin' instanceof Admin\Model\AdminModel,
+			'Merge' instanceof Think\Model\MergeModel,
+		],
+	];
+}
